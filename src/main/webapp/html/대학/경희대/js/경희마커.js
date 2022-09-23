8d2eec1b8cf5656fb2eecb5670af464ae64a9315
Min364 <mean3640@gmail.com>
2022-09-23 13:59:41 +0900

    universe

---
+++ src/main/webapp/html/대학/경희대/js/경희마커.js
@@ -0,0 +1,10 @@
+var positions = [
+    {
+        title: '경희대', 
+        latlng: new kakao.maps.LatLng(37.24288137158238,127.08119217822413)
+    },
+    {
+        title: '경희대정문', 
+        latlng: new kakao.maps.LatLng(37.24749693971308, 127.07788905753965)
+    }
+];
